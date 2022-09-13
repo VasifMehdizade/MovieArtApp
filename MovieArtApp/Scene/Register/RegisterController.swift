ef01754ef047b08bd71500416862fe5f65a9c6d5
VasifMehdizade <vasif.mehdi97@gmail.com>
2022-09-14 00:17:14 +0400

    registerController created

---
+++ MovieArtApp/Scene/Register/RegisterController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterController.swift
+//  MovieArtApp
+//
+//  Created by Vasif Mehdizade on 14.09.22.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+    @IBOutlet private weak var nameTextFieldRegister: UITextField!
+    @IBOutlet private weak var emailTextFieldRegister: UITextField!
+    @IBOutlet private weak var passwordTextFieldRegister: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func registerAction(_ sender: Any) {
+        
+    }
+    
+    @IBAction func alreadyAccountAction(_ sender: Any) {
+        
+    }
+    
+}
