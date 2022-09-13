0728bc5bae08c5f59aa1d8c081dca24221d089ce
VasifMehdizade <vasif.mehdi97@gmail.com>
2022-09-13 23:07:29 +0400

    new controller created
    Onboarding Controller

---
+++ MovieArtApp/Scene/Onboarding/OnboardingController.swift
@@ -0,0 +1,30 @@
+//
+//  OnboardingController.swift
+//  MovieArtApp
+//
+//  Created by Vasif Mehdizade on 13.09.22.
+//
+
+import UIKit
+
+class OnboardingController: UIViewController {
+
+    @IBOutlet weak var screenLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        labelEditing()
+        let delay : Double = 1.0
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            self.fireTimer()
+        }
+    }
+    func labelEditing() {
+        screenLabel.text = "Movie Art"
+        view.backgroundColor = .black
+    }
+    @objc func fireTimer() {
+        let controller = storyboard?.instantiateViewController(withIdentifier: "HomePagaController") as! HomePagaController
+        show(controller, sender: nil)
+    }
+}
